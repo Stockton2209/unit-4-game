a4c359b5184fa4485fef0c94b7e94e85077dd4bc
Alexander <alexstockton09@gmail.com>
2018-11-01 18:25:14 -0500

    pseudo coded the javascript, now for the fun part

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+ 
+
+ //red button value: 0
+ //blue button value: 0
+ //yellow button value: 0
+ //green button value: 0
+
+    var totalscore: 0;
+    var wins: 0;
+    var losses: 0;
+
+
+
+ //random number generated between 19-120
+ //each button needs random value assigned between 1-12
+    //each button clicked adds to total score
+//if total score = random number : You Win!
+    //wins counter goes up
+    //game resets (without refreshing)
+        //new random number, new button values, total score back to 0,
+//if total score > random number : You Lose!
+    //loss counter goes up
+    //game resets (without refreshing)
+        //new random number, new button values, total score back to 0,

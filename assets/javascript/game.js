a360b0347b769392c8e66be055da59e455592892
Alexander <alexstockton09@gmail.com>
2018-11-01 20:08:43 -0500

    added button value variables and created the reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,41 @@
 $(document).ready(function(){
     var targetNum = Math.floor(Math.random()*101+19);
     //generate random number at beginning
-     $('#randomNumber').text(targetNum);
+     $("#randomNumber").text(targetNum);
      //add the random number to the target number box
 
     var totalScore: 0;
     var wins: 0;
     var losses: 0;
     //variables for the counters
+
+    var hv1= Math.floor(Math.random()*11+1)
+    var hv2= Math.floor(Math.random()*11+1)
+    var hv3= Math.floor(Math.random()*11+1)
+    var hv4= Math.floor(Math.random()*11+1)
+    //variables for random button values
+
+    $("#win").text(wins);
+    $("#lose").text(losses);
+    $("#totalscore").text(totalScore);
+    //variables to update the counters
+
+    //the reset function
+    function reset(){
+        targetNum=Math.floor(Math.random()*101+19);
+        $("#randomNumber").text(targetNum);
+        //make a new random target number and display it
+        hv1= Math.floor(Math.random()*11+1);
+        hv2= Math.floor(Math.random()*11+1);
+        hv3= Math.floor(Math.random()*11+1);
+        hv4= Math.floor(Math.random()*11+1);
+        totalScore= 0;
+        $("#totalscore").text(totalScore);
+        } 
+        //make new random values for each button
+        //reset the total score and display it
+        
  
-
-
- //reset function
-    //targetNum generate new random
-    //button values reset
-    //totalScore = 0
-
-    var totalScore: 0;
-    var wins: 0;
-    var losses: 0;
-
-
-
- //random number generated between 19-120
- //each button needs random value assigned between 1-12
-    //each button clicked adds to total score
 //if total score = random number : You Win!
     //wins counter goes up
     //game resets (without refreshing)
